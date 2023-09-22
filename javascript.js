43e7a4531c8ca10a4d8a83a51b1158413c257b85
candyycode <perryc@15HPEnvyx360Lap>
2023-09-21 21:11:59 -0500

    added slidshow

--- javascript.js
+++ javascript.js
@@ -1,1 +1,31 @@
 console.log("Hello World")
+
+let slideIndex = 1;
+showslides(slideIndex);
+
+// Next/previous controls
+function plusslides(n) {
+  showslides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentslide(n) {
+  showslides(slideIndex = n);
+}
+
+function showslides(n) {
+  let i;
+  let slides = document.getElementsByClassName("myslides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
